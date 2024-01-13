b9fcb43ee118486ad56a8aaa16d7cf200e348ff0
Seasawher <tellagoodstory@icloud.com>
2024-01-13 20:28:57 +0900

    2章 5節を解く

--- MIL/C02_Basics/S05_Proving_Facts_about_Algebraic_Structures.lean
+++ MIL/C02_Basics/S05_Proving_Facts_about_Algebraic_Structures.lean
@@ -33,23 +33,84 @@ variable (x y z : α)
 #check (le_sup_right : y ≤ x ⊔ y)
 #check (sup_le : x ≤ z → y ≤ z → x ⊔ y ≤ z)
 
-example : x ⊓ y = y ⊓ x := by
-  sorry
+-- 後で使うので名前を付けておく
+lemma my_inf_comm : x ⊓ y = y ⊓ x := by
+  apply le_antisymm
+  simp
 
-example : x ⊓ y ⊓ z = x ⊓ (y ⊓ z) := by
-  sorry
+  apply le_inf
+  simp
 
-example : x ⊔ y = y ⊔ x := by
-  sorry
+  exact inf_le_left
 
-example : x ⊔ y ⊔ z = x ⊔ (y ⊔ z) := by
-  sorry
+@[simp]
+lemma inf_triple_left : x ⊓ y ⊓ z ≤ x := by
+  -- 補題を用意する
+  calc x ⊓ y ⊓ z
+    _ ≤ x ⊓ y := @inf_le_left _ _ (x ⊓ y) z
+    _ ≤ x := by apply inf_le_left
+
+@[simp]
+lemma inf_triple_mid : x ⊓ y ⊓ z ≤ y := by
+  -- 補題を用意する
+  calc x ⊓ y ⊓ z
+    _ ≤ x ⊓ y := @inf_le_left _ _ (x ⊓ y) z
+    _ ≤ y := by apply inf_le_right
+
+@[aesop safe]
+lemma my_inf_assoc : x ⊓ y ⊓ z = x ⊓ (y ⊓ z) := by
+  apply le_antisymm
+  simp
+
+  rw [my_inf_comm]
+  simp
+
+-- 後で使うので名前を付けておく
+lemma my_sup_comm : x ⊔ y = y ⊔ x := by
+  apply le_antisymm
+  simp
+
+  apply sup_le
+  simp
+
+  exact le_sup_left
+
+@[simp]
+lemma sup_triple_left : x ≤ x ⊔ y ⊔ z := by
+  -- 補題を用意する
+  calc x
+    _ ≤ x ⊔ y := le_sup_left
+    _ ≤ x ⊔ y ⊔ z := le_sup_left
+
+@[simp]
+lemma sup_triple_mid : y ≤ x ⊔ y ⊔ z := by
+  -- 補題を用意する
+  calc y
+    _ ≤ x ⊔ y := le_sup_right
+    _ ≤ x ⊔ y ⊔ z := le_sup_left
+
+@[aesop safe]
+lemma my_sup_assoc : x ⊔ y ⊔ z = x ⊔ (y ⊔ z) := by
+  apply le_antisymm
+  simp
+
+  all_goals
+    rw [my_sup_comm]
+    simp
 
 theorem absorb1 : x ⊓ (x ⊔ y) = x := by
-  sorry
+  apply le_antisymm
+  simp
+
+  apply le_inf
+  all_goals
+    simp
 
 theorem absorb2 : x ⊔ x ⊓ y = x := by
-  sorry
+  apply le_antisymm
+  simp
+
+  exact le_sup_left
 
 end
 
@@ -67,11 +128,69 @@ section
 variable {α : Type*} [Lattice α]
 variable (a b c : α)
 
+@[gcongr]
+lemma my_sup_le_sup_right (h : a ≤ b) : a ⊔ c ≤ b ⊔ c := by
+  -- 補題を用意する
+  apply sup_le
+
+  focus
+    calc a
+      _ ≤ b := h
+      _ ≤ b ⊔ c := le_sup_left
+
+  focus
+    exact le_sup_right
+
+@[gcongr]
+lemma my_inf_le_inf_right (h : a ≤ b) : a ⊓ c ≤ b ⊓ c := by
+  -- 補題を用意する
+  apply le_inf
+
+  focus
+    calc a ⊓ c
+      _ ≤ a := inf_le_left
+      _ ≤ b := h
+
+  focus
+    exact inf_le_right
+
 example (h : ∀ x y z : α, x ⊓ (y ⊔ z) = x ⊓ y ⊔ x ⊓ z) : a ⊔ b ⊓ c = (a ⊔ b) ⊓ (a ⊔ c) := by
-  sorry
+  show a ⊔ (b ⊓ c) = (a ⊔ b) ⊓ (a ⊔ c)
+  apply le_antisymm
+  all_goals simp_all
+
+  focus
+    show b ⊓ c ≤ a ⊔ ((a ⊔ b) ⊓ c)
+    rw [← @ge_iff_le]
+    calc a ⊔ (a ⊔ b) ⊓ c
+      _ ≥ (a ⊔ b) ⊓ c := le_sup_right
+    gcongr
+    exact le_sup_right
+  focus
+    show (a ⊔ b) ⊓ c ≤ a ⊔ (b ⊓ c)
+    rw [my_inf_comm]
+    rw [h]
+    rw [my_inf_comm c b]
+    gcongr
+    exact inf_le_right
 
 example (h : ∀ x y z : α, x ⊔ y ⊓ z = (x ⊔ y) ⊓ (x ⊔ z)) : a ⊓ (b ⊔ c) = a ⊓ b ⊔ a ⊓ c := by
-  sorry
+  apply le_antisymm
+  all_goals simp_all
+
+  focus
+    show a ⊓ (b ⊔ c) ≤ (a ⊓ b) ⊔ c
+    rw [my_sup_comm (a ⊓ b) c]
+    rw [h]
+    rw [my_sup_comm b _]
+    gcongr
+    exact le_sup_right
+
+  focus
+    calc a ⊓ (a ⊓ b ⊔ c)
+      _ ≤ (a ⊓ b) ⊔ c := inf_le_right
+    gcongr
+    exact inf_le_right
 
 end
 
@@ -85,13 +204,13 @@ variable (a b c : R)
 #check (mul_nonneg : 0 ≤ a → 0 ≤ b → 0 ≤ a * b)
 
 example (h : a ≤ b) : 0 ≤ b - a := by
-  sorry
+  simpa
 
 example (h: 0 ≤ b - a) : a ≤ b := by
-  sorry
+  simpa using h
 
 example (h : a ≤ b) (h' : 0 ≤ c) : a * c ≤ b * c := by
-  sorry
+  gcongr
 
 end
 
@@ -104,7 +223,11 @@ variable (x y z : X)
 #check (dist_triangle x y z : dist x z ≤ dist x y + dist y z)
 
 example (x y : X) : 0 ≤ dist x y := by
-  sorry
+  have := calc
+    0 = dist x x := by rw [dist_self x]
+    _ ≤ dist x y + dist y x := dist_triangle x y x
+    _ = dist x y + dist x y := by rw [dist_comm x y]
+    _ = 2 * dist x y := by ring
+  simpa using this
 
 end
-
