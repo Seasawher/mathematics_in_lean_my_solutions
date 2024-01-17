0558a2ddbca138471b8867b4b6a6c407b47ca77b
Seasawher <tellagoodstory@icloud.com>
2024-01-18 00:18:24 +0900

    3節の演習を完了

--- MIL/C03_Logic/S03_Negation.lean
+++ MIL/C03_Logic/S03_Negation.lean
@@ -66,30 +66,32 @@ example (h : a ≤ b) (h' : f b < f a) : ¬Monotone f := by
 
 example : ¬∀ {f : ℝ → ℝ}, Monotone f → ∀ {a b}, f a ≤ f b → a ≤ b := by
   intro h
-  let f := fun x : ℝ ↦ (0 : ℝ)
+  let f := fun _ : ℝ ↦ (0 : ℝ)
   have monof : Monotone f := by intro x y h; simp [f]
   have h' : f 1 ≤ f 0 := le_refl _
-  sorry
+  replace h := @h f monof 1 0 h'
+  linarith
 
 example (x : ℝ) (h : ∀ ε > 0, x < ε) : x ≤ 0 := by
-  sorry
-
+  contrapose! h
+  use x / 2
+  constructor <;> linarith
 end
 
 section
 variable {α : Type*} (P : α → Prop) (Q : Prop)
 
 example (h : ¬∃ x, P x) : ∀ x, ¬P x := by
-  sorry
+  aesop
 
 example (h : ∀ x, ¬P x) : ¬∃ x, P x := by
-  sorry
+  aesop
 
 example (h : ¬∀ x, P x) : ∃ x, ¬P x := by
-  sorry
+  aesop
 
 example (h : ∃ x, ¬P x) : ¬∀ x, P x := by
-  sorry
+  aesop
 
 example (h : ¬∀ x, P x) : ∃ x, ¬P x := by
   by_contra h'
@@ -100,10 +102,10 @@ example (h : ¬∀ x, P x) : ∃ x, ¬P x := by
   exact h' ⟨x, h''⟩
 
 example (h : ¬¬Q) : Q := by
-  sorry
+  tauto
 
 example (h : Q) : ¬¬Q := by
-  sorry
+  tauto
 
 end
 
@@ -111,7 +113,9 @@ section
 variable (f : ℝ → ℝ)
 
 example (h : ¬FnHasUb f) : ∀ a, ∃ x, f x > a := by
-  sorry
+  dsimp [FnHasUb, FnUb] at h
+  push_neg at h
+  assumption
 
 example (h : ¬∀ a, ∃ x, f x > a) : FnHasUb f := by
   push_neg at h
@@ -123,7 +127,9 @@ example (h : ¬FnHasUb f) : ∀ a, ∃ x, f x > a := by
   exact h
 
 example (h : ¬Monotone f) : ∃ x y, x ≤ y ∧ f y < f x := by
-  sorry
+  dsimp [Monotone] at h
+  push_neg at h
+  exact h
 
 example (h : ¬FnHasUb f) : ∀ a, ∃ x, f x > a := by
   contrapose! h
@@ -147,7 +153,7 @@ example (h : 0 < 0) : a > 37 :=
   absurd h (lt_irrefl 0)
 
 example (h : 0 < 0) : a > 37 := by
-  have h' : ¬0 < 0 := lt_irrefl 0
+  have _ : ¬0 < 0 := lt_irrefl 0
   contradiction
 
 end
